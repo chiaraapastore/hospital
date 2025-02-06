23bd19582739300bfda6aa524dd6666636da56f9
chiaraapastore <chiarapastoree001@gmail.com>
2025-02-06 19:22:17 +0100

    Initial commit

--- src/main/java/hospitalApplication/repository/DepartmentRepository.java
+++ src/main/java/hospitalApplication/repository/DepartmentRepository.java
@@ -4,9 +4,7 @@ import hospitalApplication.models.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 
 @Repository
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
-    Optional<Department> findByNome(String repartoDefault);
 }
